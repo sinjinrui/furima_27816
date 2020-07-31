6f3989d76a1b66f7103035ad0b1e8667261c9933
sinjinrui <66107113+sinjinrui@users.noreply.github.com>
2020-07-31 12:11:45 +0900

    fix_cord

--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -7,52 +7,49 @@ describe Product do
 
     context '登録されない' do
 
+      before do
+        @product = build(:product, name: nil, explanation: nil, category_id: 1, status_id: 1, 
+                        delivery_fee_id: 1, shipping_origin_id: 1, until_shipping_id: 1, price: nil)
+      end
+
       it '商品名が空だと登録されない' do
-        product = build(:product, name: nil)
-        product.valid?
-        expect(product.errors[:name]).to include("を入力してください")
+        @product.valid?
+        expect(@product.errors[:name]).to include("を入力してください")
       end
 
       it '商品の説明が空だと登録されない' do
-        product = build(:product, explanation: nil)
-        product.valid?
-        expect(product.errors[:explanation]).to include("を入力してください")
+        @product.valid?
+        expect(@product.errors[:explanation]).to include("を入力してください")
       end
 
       it 'category_idが1だと登録されない' do
-        product = build(:product, category_id: 1)
-        product.valid?
-        expect(product.errors[:category_id]).to include("を選んでください")
+        @product.valid?
+        expect(@product.errors[:category_id]).to include("を選んでください")
       end
 
       it 'status_idが1だと登録されない' do
-        product = build(:product, status_id: 1)
-        product.valid?
-        expect(product.errors[:status_id]).to include("を選んでください")
+        @product.valid?
+        expect(@product.errors[:status_id]).to include("を選んでください")
       end
 
       it 'delivery_fee_idが1だと登録されない' do
-        product = build(:product, delivery_fee_id: 1)
-        product.valid?
-        expect(product.errors[:delivery_fee_id]).to include("を選んでください")
+        @product.valid?
+        expect(@product.errors[:delivery_fee_id]).to include("を選んでください")
       end
 
       it 'shipping_origin_idが1だと登録されない' do
-        product = build(:product, shipping_origin_id: 1)
-        product.valid?
-        expect(product.errors[:shipping_origin_id]).to include("を選んでください")
+        @product.valid?
+        expect(@product.errors[:shipping_origin_id]).to include("を選んでください")
       end
 
       it 'until_shipping_idが1だと登録されない' do
-        product = build(:product, until_shipping_id: 1)
-        product.valid?
-        expect(product.errors[:until_shipping_id]).to include("を選んでください")
+        @product.valid?
+        expect(@product.errors[:until_shipping_id]).to include("を選んでください")
       end
 
       it '価格が空だと登録されない' do
-        product = build(:product, price: nil)
-        product.valid?
-        expect(product.errors[:price]).to include("を入力してください")
+        @product.valid?
+        expect(@product.errors[:price]).to include("を入力してください")
       end
 
       it '価格が299未満だと登録されない' do
