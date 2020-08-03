a39dbb8a445c28a0e9af340735000741988b4568
sinjinrui <66107113+sinjinrui@users.noreply.github.com>
2020-08-03 11:33:11 +0900

    complete_delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_product, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
     @products = Product.all.order(id: "DESC")
@@ -33,6 +33,11 @@ class ProductsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
   end
 
   private
