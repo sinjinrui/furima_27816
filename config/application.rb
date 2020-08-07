e6c02a8fdb9071f1e758da3d447c4ac5bce64fe9
sinjinrui <66107113+sinjinrui@users.noreply.github.com>
2020-08-07 15:12:31 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Furima27816
     config.i18n.default_locale = :ja
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
+    config.autoloader = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
